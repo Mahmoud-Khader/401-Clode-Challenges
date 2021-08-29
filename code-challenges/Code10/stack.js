3dcb672dc65713d18148ef4b79532c50543140b0
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-08-29 15:32:02 +0300

    done Code 11

--- code-challenges/Code10/stack.js
+++ code-challenges/Code10/stack.js
@@ -1,46 +1,45 @@
-"use strict";
+'use strict'
+const Node=require('./node')
+class Stack{
+    constructor(){
+        this.top=null;
 
-const Node = require("./node");
-
-class Stack {
-  constructor() {
-    this.top = null;
-  }
-
-  push(value) {
-    const node = new Node(value);
-    if (this.top) {
-      node.next = this.top;
     }
-    this.top = node;
-  }
 
-  pop() {
-    try {
-      let temp = this.top;
-      this.top = this.top.next;
-      temp.next = null;
-      return temp.value;
-    } catch {
-      return "stack is empty";
-    }
-  }
 
-  peek() {
-    try {
-      return this.top.value;
-    } catch {
-      return "stack is empty";
-    }
-  }
+    push(value){
+        let newNode=new Node(value)
+        if(this.top){
+            newNode.next=this.top
+            this.top=newNode
+        }
+        this.top=newNode
 
-  isEmpty() {
-    if (this.top) {
-      return false;
-    } else {
-      return true;
+        
     }
-  }
+
+    pop(){
+        if(!this.top) return 'there is no nodes'
+        let top =this.top 
+        this.top=this.top.next
+        return top.value
+        
+    }
+    peek(){
+        if(!this.top) return -1 
+        return this.top.value
+    }
+    isEmpty(){
+        if(!this.top){
+            return true
+        }
+    
+          return  false
+        
+    }
+
 }
+
+
 
 module.exports = Stack;
---
+++ code-challenges/Code11/Readme.md
@@ -0,0 +1,40 @@
+# Stack-and-Queue
+
+**1- Stack :**
+  - create a stack class that has a top property ,It create an empty 
+   - Stack when instantiated
+
+The object should be oware of a default empty value assigned toTopwhn the stack created .
+
+**2- Queue :** 
+
+- Create a Queue class that has a front property , It create an empty Queue when instantiated .
+- this object should be aware of a default empty value assigned 
+- toFront when the is created
+
+## Whiteboard Process
+![stack-and-queue](../images/stack-and-queue.PNG)
+
+## Approach & Efficiency
+* *i undertand the problem first*
+* *I imagined how the results should be*
+* *I wrote the code*
+* *I made the tests*
+
+## API
+
+**stack**
+
+* *push(value) : adds a new node with that value to the top of the stack with an O(1) Time performance.*
+* *pop() : Removes the node from the top of the stack*
+* *peek() : Returns the Value of the node located at the top of the stack*
+* *isEmpty() : Returns a boolean indicating whether or not the stack is empty.*
+
+**queue**
+
+* *enqueue(value) : adds a new node with that value to the back of the queue*
+* *with an O(1) Time performance.*
+* *dequeue() : Removes the node from the front of the queue*
+* *peek() : Returns the Value of the node located at the front of the queue*
+* *isEmpty() : Returns a boolean indicating whether or not the queue is empty*
+
